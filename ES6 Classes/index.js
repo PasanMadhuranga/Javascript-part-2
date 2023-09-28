6d5c50265397da9d907d597d938337f3eb9886cc
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2023-09-28 07:41:25 +0530

    videos 3 - 9

--- ES6 Classes/index.js
+++ ES6 Classes/index.js
@@ -50,3 +50,133 @@ class Circle3 {
 }
 
 const circle = Circle3.parse('{"radius": 1}'); // Static methods are called without creating an object of the class
+
+
+// 4 - This Keyword 
+class Circle4 {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    draw() {
+        console.log(this);
+    }
+}
+
+const c4 = new Circle4(12);
+// Method Call
+c4.draw(); // Circle4 { radius: 12 }
+
+const draw = c4.draw;
+// Function Call
+draw(); // This will return undefined because by default body of classes are executed in strict mode 
+
+
+// 5 - Private Members Using Symbols
+const _radius = Symbol();
+const _draw = Symbol();
+
+class Circle5 {
+    constructor(radius) {
+        this[_radius] = radius;
+    }
+
+    [_draw]() {
+        console.log('draw');
+    }
+}
+
+const c5 = new Circle5(23);
+console.log(c5); 
+
+
+// 6 - Private Members Using WeakMaps
+const _radius2 = new WeakMap();
+const _move = new WeakMap();
+
+class Circle6 {
+    constructor(radius) {
+        _radius2.set(this, radius);
+        _move.set(this, () => {
+            console.log('move', this);
+        });
+    }
+
+    draw() {
+        _move.get(this)();
+        console.log('draw');
+    }
+}
+
+const c6 = new Circle6(4);
+console.log(c6); // Circle6 {}
+console.log(_radius2.get(c6)); // 4
+c6.draw(); // move Circle6 {} draw
+
+
+// 7 - Getters and Setters
+const _radius3 = new WeakMap();
+ class Circle7 {
+    constructor(radius) {
+        _radius3.set(this, radius);
+    }
+
+    get radius() {
+        return _radius3.get(this);
+    }
+
+    set radius(value) {
+        if (value <= 0) throw new Error('Invalid radius');
+        _radius3.set(this, value);
+    }
+}
+
+const c7 = new Circle7(1);
+console.log(c7.radius); // 1
+c7.radius = 10;
+console.log(c7.radius); // 10
+// c7.radius = -1; // Error: Invalid radius
+
+
+// 8 - Inheritance
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+
+    move() {
+        console.log('move');
+    }
+}
+
+class Circle8 extends Shape {
+    constructor(color, radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    draw() {
+        console.log('draw');
+    }
+}
+
+const c8 = new Circle8('green', 4);
+console.log(c8); // Circle8 { color: 'green', radius: 4 }
+
+
+// 9 - Method Overriding
+class Shape2 {
+    move() {
+        console.log('move');
+    }
+}
+
+class Circle9 extends Shape2 {
+    move() {
+        super.move();
+        console.log('circle move');
+    }
+}
+
+const c9 = new Circle9();
+c9.move(); // move circle move
